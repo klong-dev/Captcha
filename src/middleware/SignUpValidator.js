ffaf0d99454b23206ac8c5d1ed565b77a6b07f6c
viethoang1520 <vhd151020@gmail.com>
2024-05-26 11:42:29 +0700

    delete confirm password field

--- src/middleware/SignUpValidator.js
+++ src/middleware/SignUpValidator.js
@@ -6,7 +6,6 @@ const validateForm = [
   check("username", "Username must be at least 4 characters long").isLength({
     min: 4,
   }),
-  // check("phone", "Phone number is not correct ").matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/g),
   check("password", "Please enter a password").notEmpty(),
   check("password", "Password must be at least 8 characters long").isLength({
     min: 8,
@@ -16,15 +15,6 @@ const validateForm = [
     "Password must contain at least 1 letter"
   ).matches(/^[\w.]+/),
 
-  check("confirm-password", "Please enter a confirmation password").notEmpty(),
-  check("confirm-password", "Confirmation password does not match").custom(
-    (value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Confirmation password does not match");
-      }
-      return true;
-    }
-  ),
 ];
 
 module.exports = {
