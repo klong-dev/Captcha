cd3aaf405038292a5fbbdaacf057595c26a0e409
viethoang1520 <vhd151020@gmail.com>
2024-05-29 17:44:29 +0700

    delete redundant files

--- src/routes/captcha.js
+++ src/routes/captcha.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const captchaController = require("../app/controllers/captchaController");
 
 router.post("/pay", captchaController.pay);
-// router.get("/solve", captchaController.solve);
 router.post("/solve", captchaController.solve);
 
 module.exports = router;
